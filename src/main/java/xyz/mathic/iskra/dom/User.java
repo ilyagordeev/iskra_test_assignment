76883db7c35c68ace44be62d297262802e0819f7
Ilya Gordeev <ilya.tanat@gmail.com>
2020-01-25 22:03:22 +0500

    add key field

--- src/main/java/xyz/mathic/iskra/dom/User.java
+++ src/main/java/xyz/mathic/iskra/dom/User.java
@@ -11,9 +11,17 @@ public class User {
 
     private String name;
     private Status status;
+    private int key;
+    private static int count = 0;
+
+
+    public int getKey() {
+        return key;
+    }
 
     public User(String name) {
         this.name = name;
+        this.key = count++;
         this.setStatus(Status.OFFLINE);
     }
 
